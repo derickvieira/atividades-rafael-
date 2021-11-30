f7e454fcdc4155e1a9324d572e61d121e3cccae5
Derick <94722957+derickvieira@users.noreply.github.com>
2021-11-30 20:43:54 -0300

    ativdade 1

---
+++ primeiro projeto.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+ main() {
+         float a, b, c, d, tr;
+         printf("Digite a primeira nota: ");  
+         scanf("%f",&a);
+         printf("Digite a segunda nota: "); 
+         scanf("%f",&b); 
+         printf("Digite a terceira nota: ");
+         scanf("%f",&c);
+         printf("Digite a quarta nota: ");
+         scanf("%f",&d);
+         
+         tr = ("%f", a + b + c + d)/4;
+         
+         
+         printf("O valor da media é: %.1f \n",tr);
+         
+         system("pause");    
+        }        
+    
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
